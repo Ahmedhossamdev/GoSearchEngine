79c8a36f5738f1fd02bdec0e416f7658f141a3f5
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-07-09 01:41:51 +0300

    chore: Add User model with necessary fields and annotations

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        string     `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
+	Email     string     `gorm:"unique" json:"email"`
+	Password  string     `json:"-"`
+	IsAdmin   bool       `gorm:"default:false" json:"isAdmin"`
+	CreatedAt *time.Time `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt"`
+}
